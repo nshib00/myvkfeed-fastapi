5df93a54d1c1a0d0944010b1c0f27f2db44e7fde
nshib <nshib54321@gmail.com>
2024-08-12 11:14:42 +0300

    Add menu and user in templates, auto-fill version in footer

---
+++ app/__init__.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+import toml
+
+version = "unknown"
+# adopt path to your pyproject.toml
+pyproject_toml_file = Path(__file__).parent.parent / "pyproject.toml"
+if pyproject_toml_file.exists() and pyproject_toml_file.is_file():
+    data = toml.load(pyproject_toml_file)
+    # check project.version
+    if "project" in data and "version" in data["project"]:
+        version = data["project"]["version"]
+    # check tool.poetry.version
+    elif "tool" in data and "poetry" in data["tool"] and "version" in data["tool"]["poetry"]:
+        version = data["tool"]["poetry"]["version"]
+        
+__version__ = version
--- app/pages/router.py
+++ app/pages/router.py
@@ -4,7 +4,10 @@ from fastapi.templating import Jinja2Templates
 from app.groups.router import get_all_groups_to_render, get_group_by_id
 from app.groups.schemas import GroupSchema, GroupSchemaWithPosts
 from app.posts.router import get_all_posts_to_render, get_post_by_id
-from app.posts.schemas import PostResponseRenderSchema, PostResponseSchemaWithImages
+from app.posts.schemas import PostResponseRenderSchema
+from app.users.auth.dependencies import get_active_current_user
+from app.users.models import Users
+from app import __version__
 
 
 router = APIRouter(
@@ -15,46 +18,74 @@ router = APIRouter(
 templates = Jinja2Templates(directory='app/templates')
 
 
+base_context = {
+    'menu': [
+        {'title': 'Обновить ленту', 'url': ''},
+        {'title': 'Мои группы', 'url': '/pages/groups'},
+    ],
+    'version': __version__,
+    'year': '2024',
+}
+
+
 @router.get('')
-async def get_main_page(request: Request, posts: list[PostResponseRenderSchema] = Depends(get_all_posts_to_render)):
+async def get_main_page(
+    request: Request,
+    posts: list[PostResponseRenderSchema] = Depends(get_all_posts_to_render),
+    user: Users = Depends(get_active_current_user)
+):
     return templates.TemplateResponse(
         name='index.html',
-        context={
+        context=base_context | {
             'request': request,
             'posts': posts,
+            'user': user,
         }
     )
 
 @router.get('/post/{post_id}')
-async def get_post_page(request: Request, post: PostResponseRenderSchema = Depends(get_post_by_id)):
+async def get_post_page(
+    request: Request,
+    post: PostResponseRenderSchema = Depends(get_post_by_id),
+    user: Users = Depends(get_active_current_user)
+):
     return templates.TemplateResponse(
         name='post.html',
-        context={
+        context=base_context | {
             'request': request,
             'post': post,
+            'user': user,
         }
     )
 
 
 @router.get('/groups')
-async def get_all_groups_page(request: Request, groups: list[GroupSchema] = Depends(get_all_groups_to_render)):
+async def get_all_groups_page(
+    request: Request,
+    groups: list[GroupSchema] = Depends(get_all_groups_to_render),
+    user: Users = Depends(get_active_current_user)
+):
     return templates.TemplateResponse(
         name='all_groups.html',
-        context={
+        context=base_context | {
             'request': request,
             'groups': groups,
+            'user': user,
         }
     )
 
 
 @router.get('/group/{group_id}')
 async def get_group_page(
-    request: Request, group: GroupSchemaWithPosts = Depends(get_group_by_id)
+    request: Request,
+    group: GroupSchemaWithPosts = Depends(get_group_by_id),
+    user: Users = Depends(get_active_current_user)
 ):
     return templates.TemplateResponse(
         name='group.html',
-        context={
+        context=base_context | {
             'request': request,
             'group': group,
+            'user': user,
         }
     )
