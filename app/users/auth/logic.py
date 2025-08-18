47e2d73690c666245867688247ca7ef461072931
Nikita Shibaev <nshib.54321@gmail.com>
2025-08-18 10:51:13 +0300

    Fix in token creating

--- app/users/auth/logic.py
+++ app/users/auth/logic.py
@@ -14,7 +14,7 @@ from app.users.auth.password_hash import HashPassword
 from app.users.service import UserService
 
 
-def create_token(token_data: dict, token_type: str) -> str:
+def create_token(token_data: dict, token_type: str) -> tuple[str, datetime]:
     data_to_encode = token_data.copy()
     if token_type == ACCESS_TOKEN_TYPE:
         expire = datetime.now(timezone.utc) + timedelta(minutes=settings.auth.ACCESS_TOKEN_EXPIRE_MINUTES)
@@ -30,26 +30,28 @@ def create_token(token_data: dict, token_type: str) -> str:
         'type': token_type
     }
     encoded_jwt = jwt.encode(data_to_encode, settings.auth.SECRET_KEY, algorithm=settings.auth.ALGORITHM)
-    return encoded_jwt
+    return encoded_jwt, expire
 
 
 def create_access_and_refresh_tokens(token_data: dict) -> TokenInfo:
-    access_token = create_token(token_data=token_data, token_type=ACCESS_TOKEN_TYPE)
-    refresh_token = create_token(token_data=token_data, token_type=REFRESH_TOKEN_TYPE)
+    access_token, _ = create_token(token_data=token_data, token_type=ACCESS_TOKEN_TYPE)
+    refresh_token, refresh_expire = create_token(token_data=token_data, token_type=REFRESH_TOKEN_TYPE)
     return TokenInfo(
         access_token=access_token,
-        refresh_token=refresh_token
+        refresh_token=refresh_token,
+        refresh_token_expires_at=refresh_expire
     )
 
 
 async def create_and_save_tokens(response: Response, user: Users = Depends(get_active_current_user)) -> TokenInfo:
-    user_token_data = {'sub': user.id}
-    tokens = create_access_and_refresh_tokens(token_data=user_token_data)
+    user_token_data = {'sub': str(user.id)}
+    tokens: TokenInfo = create_access_and_refresh_tokens(token_data=user_token_data)
     response.set_cookie('myvkfeed_access_token', tokens.access_token, httponly=True)
     await RefreshTokenService.add(
         id=uuid4(),
         user_id=user.id,
         token=tokens.refresh_token,
+        expires_at=tokens.refresh_token_expires_at,
     )
     return tokens
     
@@ -58,7 +60,7 @@ async def authenticate_user(username: str | None, password: str | None) -> Users
     existing_user = await UserService.find_one_or_none(name=username)
     if existing_user is None:
         raise UserNotExistsException
-    if not HashPassword.verify_password(password, existing_user.hashed_password):
+    if password is None or not HashPassword.verify_password(password, existing_user.hashed_password):
         raise IncorrectCredentialsException
     return existing_user
 
