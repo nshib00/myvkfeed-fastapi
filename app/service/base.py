0f9998309d2bfa367374f26dabfe416644e00b7e
Nikita Shibaev <nshib.54321@gmail.com>
2025-08-21 11:56:47 +0300

    Add model check in BaseService

--- app/service/base.py
+++ app/service/base.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Delete, Insert, MappingResult, Result, ScalarResult, Update, delete, insert, select, text, update
+from sqlalchemy import Delete, Insert, Result, ScalarResult, TextClause, Update, delete, insert, select, text, update
 
 from app.database import async_sessionmaker
 
@@ -8,6 +8,8 @@ class BaseService:
 
     @classmethod
     async def _get_result(cls, order_by_id: bool = False, **filters) -> ScalarResult:
+        if cls.model is None:
+            raise ValueError('Model in service should not be None')
         query = select(cls.model).filter_by(**filters)
         if order_by_id:
             query = query.order_by(cls.model.id)
@@ -16,7 +18,7 @@ class BaseService:
             return result.scalars()
     
     @classmethod
-    async def _execute_with_commit(cls, query: Insert | Update | Delete) -> Result:
+    async def _execute_with_commit(cls, query: Insert | Update | Delete | TextClause) -> Result:
         async with async_sessionmaker() as session:
             result = await session.execute(query)
             await session.commit()
@@ -33,30 +35,32 @@ class BaseService:
         return result.one_or_none() 
         
     @classmethod
-    async def add(cls, **data) -> int:
+    async def add(cls, **data):
+        if cls.model is None:
+            raise ValueError('Model in service should not be None')
         query = insert(cls.model).values(**data).returning(cls.model.id)
         result = await cls._execute_with_commit(query)
         return result.scalar()
     
 
     @classmethod
-    async def update(cls, model, update_condition, **values) -> int:
+    async def update(cls, model, update_condition, **values):
         query = update(model).where(update_condition).values(**values).returning(model.id)
         result = await cls._execute_with_commit(query)
         return result.scalar()
     
-
     @classmethod
     async def delete(cls, delete_condition=None, model=None) -> None:
-        if cls.model is None:
-            cls.model = model
+        model_to_delete_from = cls.model or model
+        if model_to_delete_from is None:
+            raise ValueError('Model in service should not be None')
         if delete_condition is not None:
-            query = delete(cls.model).where(delete_condition)
+            query = delete(model_to_delete_from).where(delete_condition)
         await cls._execute_with_commit(query)
-    
 
     @classmethod
     async def delete_all(cls) -> None:
+        if cls.model is None:
+            raise ValueError('Model in service should not be None')
         query = text(f'TRUNCATE TABLE {cls.model.__tablename__} CASCADE')
         await cls._execute_with_commit(query)
-    
